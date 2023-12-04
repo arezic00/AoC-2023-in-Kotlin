139318e3e46b38c3247ac241aa9b471d4f20b8f4
Antonio Rezic <antonio.rezic00@gmail.com>
2023-12-04 18:08:03 +0100

    Optimize Card data class

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,6 +1,6 @@
 import kotlin.math.pow
 
-data class Card(val id: Int, val winningNumbers: List<Int>, val numbers: List<Int>)
+data class Card(val id: Int, val matches: Int)
 fun main() {
     val test = Day04(readInput("Day04_test"))
     check(test.part1() == 13)
@@ -21,19 +21,17 @@ class Day04(lines: List<String>) {
             .removePrefix(": ")
             .split(" | ")
             .map { List(it.trim()) }
-        return Card(id, numberSets[0], numberSets[1])
+        return Card(id, numberSets[0].filter { numberSets[1].contains(it) }.size)
     }
 
     private fun List(numberSetString: String) : List<Int> =
         numberSetString.split("\\s+".toRegex()).map { it.toInt() }
 
-    private fun Card.matches() : Int = this.winningNumbers.filter { numbers.contains(it) }.size
-
-    private fun Card.points() : Int = 2.0.pow(this.matches() - 1).toInt()
+    private fun Card.points() : Int = 2.0.pow(this.matches - 1).toInt()
 
     fun part1() : Int = cards.sumOf { it.points() }
 
-    private fun Card.winsCardsWithSelf(): Int = (0 until this.matches()).sumOf {
+    private fun Card.winsCardsWithSelf(): Int = (0 until this.matches).sumOf {
             cards[this.id + it].winsCardsWithSelf() } + 1
 
     fun part2() : Int = cards.sumOf { it.winsCardsWithSelf() }
