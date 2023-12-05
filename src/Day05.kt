561128ff109d8373d29ffbcd744c80e524a46470
Antonio Rezic <antonio.rezic00@gmail.com>
2023-12-05 14:27:09 +0100

    Complete Day05 part 2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,17 +1,16 @@
-import java.math.BigInteger
-
 fun main() {
     fun part1(input: List<String>): Int {
         return Day05().part1(input).toInt()
-    }
+   }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return Day05().part2(input).toInt()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 35)
+    check(part2(testInput) == 46)
 
 
     val input = readInput("Day05")
@@ -38,15 +37,19 @@ class Day05 {
 
     //need to read seeds - the map in every category
 
-    private var seeds: List<BigInteger> = listOf()
+    private var seeds: List<UInt> = listOf()
     private var maps: List<Map> = mutableListOf()
+    private var seeds2: List<Pair<UInt,UInt>> = mutableListOf()
 
-    private fun parseSeeds(line: String): List<BigInteger> = line.removePrefix("seeds: ").split(" ").map { it.toBigInteger() }
-    fun parse(lines: List<String>) {
-        seeds = parseSeeds(lines.first())
+    private fun parseSeeds1(line: String): List<UInt> =
+        line.removePrefix("seeds: ").split(" ").map { it.toUInt() }
+    private fun parseSeeds2(line: String) : List<Pair<UInt,UInt>> =
+        parseSeeds1(line).chunked(2) {Pair(it.first(),it.first() + it.last() - 1u)}
+
+    private fun parse(lines: List<String>) {
         val mapStrings = lines.drop(3)
         val tempMaps = mutableListOf<Map>()
-        var rows = mutableListOf<Triple<BigInteger,BigInteger,BigInteger>>()
+        var rows = mutableListOf<Triple<UInt,UInt,UInt>>()
         for ((index,line ) in mapStrings.withIndex()) {
             if (line.isNotEmpty() && line.first().isDigit()) {
                 rows.add(Triple(line))
@@ -61,30 +64,65 @@ class Day05 {
         }
         maps = tempMaps
     }
-    private fun seedToLocation(seed: BigInteger): BigInteger {
+    private fun seedToLocation(seed: UInt): UInt {
         var location = seed
         maps.forEach { location = it.map(location) }
         return location
     }
 
-    fun part1(lines: List<String>) : BigInteger {
+    fun part1(lines: List<String>) : UInt {
+        seeds = parseSeeds1(lines.first())
         parse(lines)
         return seeds.minOf { seedToLocation(it) }
     }
 
-    private fun Triple(line: String) : Triple<BigInteger,BigInteger,BigInteger> {
-        val list = line.split(" ").map { it.toBigInteger() }
+    private fun Triple(line: String) : Triple<UInt,UInt,UInt> {
+        val list = line.split(" ").map { it.toUInt() }
         return  Triple(list[0],list[1],list[2])
     }
+
+    fun part2(lines: List<String>) : UInt {
+        seeds2 = parseSeeds2(lines.first())
+        seeds2.println()
+        parse(lines)
+        "parsed".println()
+        var min = UInt.MAX_VALUE
+        seeds2.forEach {
+            for (seed in Seeds2Iterator(it) ) {
+                val location = seedToLocation(seed)
+                if (location < min)
+                    min = location
+            }
+            kotlin.io.println("Completed seeds2 from ${it.first}")
+        }
+        min.println()
+        return min
+    }
+
+
 
 
 }
 
-class Map(val rows: MutableList<Triple<BigInteger,BigInteger,BigInteger>>) {
-    fun map(input: BigInteger) : BigInteger {
+class Map(val rows: MutableList<Triple<UInt,UInt,UInt>>) {
+    fun map(input: UInt) : UInt {
         rows.forEach {
-            if (input in (it.second ..(it.second + it.third).minus(BigInteger.ONE)))
+            if (input in (it.second ..(it.second + it.third) - 1u))
                 return it.first - it.second + input}
         return input
+    }
+}
+
+class Seeds2Iterator(private val range:Pair<UInt,UInt>) : Iterator <UInt> {
+    private var current = range.first
+    override fun hasNext(): Boolean {
+        return current <= range.second
+    }
+
+    override fun next(): UInt {
+        if (!hasNext()) {
+            throw NoSuchElementException()
+        }
+        return current++
     }
 }
