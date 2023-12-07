622438ba424a066b01c6c4a5cf6b6bd6bbc7e981
Antonio Rezic <antonio.rezic00@gmail.com>
2023-12-07 11:47:30 +0100

    Refactor and beautify

--- src/Day07.kt
+++ src/Day07.kt
@@ -20,29 +20,24 @@ fun main() {
 }
 
 object Day07 {
-    fun part1(lines: List<String>) : Int {
-        val hands = lines.map {
-            Hand(it)
-        }
-        val handsResult = hands.sortedBy { it.strength() }
-        return handsResult.mapIndexed { index, hand -> (index + 1) * hand.bid }.sum()
-    }
+    private const val BASE_TYPE_STRENGTH = 1000000
+    private const val BASE_CARD_STRENGTH = 13.0
+    fun part1(lines: List<String>) = lines.map { Hand(it) }
+        .sortedBy { it.strength() }
+        .mapIndexed { index, hand -> (index + 1) * hand.bid }.sum()
 
-    fun part2(lines: List<String>) : Int {
-        val hands = lines.map {
-            Hand(it)
-        }
-        val handsResult = hands.sortedBy { it.strength2() }
-        return handsResult.mapIndexed { index, hand -> (index + 1) * hand.bid }.sum()
-    }
+    fun part2(lines: List<String>) = lines.map { Hand(it) }
+        .sortedBy { it.strength2() }
+        .mapIndexed { index, hand -> (index + 1) * hand.bid }.sum()
+
     private fun Hand(string: String): Hand {
         val strings = string.split(" ")
-        val cards = strings.first().map { Card(it.toLabel()) }
+        val cards = strings.first().map { Card(it) }
         val bid = strings.last().toInt()
         return Hand(bid, cards)
     }
     private fun Hand.type(): Type {
-        val counts = Label.values().map { this.cards.count { card -> card.label == it } }
+        val counts = Card.values().map { card -> this.cards.count { it == card } }
         if (counts.contains(5)) return Type.FIVE_OF_A_KIND
         if (counts.contains(4)) return Type.FOUR_OF_A_KIND
         if (counts.contains(3) && counts.contains(2)) return Type.FULL_HOUSE
@@ -52,76 +47,64 @@ object Day07 {
         return Type.HIGH_CARD
     }
 
-    //Ordinal of joker is 9
     private fun Hand.type2(): Type {
-        val labels = Label.values().toMutableList()
+        val labels = Card.values().toMutableList()
         labels.removeAt(9)
-        val jokerCount = cards.count { it.label == Label.J }
-        val counts = labels.map { cards.count { card -> card.label == it } }
-        if (counts.contains(5)) return Type.FIVE_OF_A_KIND
-        if (counts.contains(4))
-            return if (jokerCount > 0) Type.FIVE_OF_A_KIND else Type.FOUR_OF_A_KIND
+        val jokerCount = cards.count { it == Card.J }
+        if (jokerCount == 0) return this.type()
+
+        val counts = labels.map { card -> cards.count { it == card } }
+        if (jokerCount > 3 || counts.contains(4) || counts.contains(5))
+            return Type.FIVE_OF_A_KIND
+
         if (counts.contains(3) && counts.contains(2))
             return when (jokerCount) {
-                0 -> Type.FULL_HOUSE
                 1 -> Type.FOUR_OF_A_KIND
                 else -> Type.FIVE_OF_A_KIND
             }
         if (counts.contains(3))
             return when (jokerCount) {
-                0 -> Type.THREE_OF_A_KIND
                 1 -> Type.FOUR_OF_A_KIND
                 else -> Type.FIVE_OF_A_KIND
             }
         if (counts.count { it == 2} == 2 )
             return when (jokerCount) {
-                0 -> Type.TWO_PAIR
                 1 -> Type.FULL_HOUSE
                 2 -> Type.FOUR_OF_A_KIND
                 else -> Type.FIVE_OF_A_KIND
             }
         if (counts.contains(2))
             return when (jokerCount) {
-                0 -> Type.ONE_PAIR
                 1 -> Type.THREE_OF_A_KIND
                 2 -> Type.FOUR_OF_A_KIND
                 else -> Type.FIVE_OF_A_KIND
             }
-        //0->OP, 1->TH, 2->FR, 3->FV
         return when (jokerCount) {
-            0 -> Type.HIGH_CARD
             1 -> Type.ONE_PAIR
             2 -> Type.THREE_OF_A_KIND
-            3 -> Type.FOUR_OF_A_KIND
-            else -> Type.FIVE_OF_A_KIND
+            else -> Type.FOUR_OF_A_KIND
         }
     }
+    private fun Hand.strength(): Int = type().ordinal * BASE_TYPE_STRENGTH + cards.cardLabelStrength()
 
-    private fun Hand.strength(): Int = (type().ordinal + 1) * 1000000 + cards.cardLabelStrength()
-
-    private fun Hand.strength2(): Int = (type2().ordinal + 1) * 1000000 + cards.cardLabelStrength2()
+    private fun Hand.strength2(): Int = this.type2().ordinal * BASE_TYPE_STRENGTH + cards.cardLabelStrength2()
 
 
     private fun List<Card>.cardLabelStrength() = reversed()
-        .mapIndexed { index, card -> (card.label.ordinal + 1) * 13.0.pow(index).toInt() }
+        .mapIndexed { index, card -> (card.ordinal) * BASE_CARD_STRENGTH.pow(index).toInt() }
         .sum()
 
     private fun List<Card>.cardLabelStrength2() = reversed()
-        .mapIndexed { index, card ->
-        if (card.label == Label.J)
-            0
-        else
-            (card.strength2()) * 13.0.pow(index).toInt()}
+        .mapIndexed { index, card -> (card.strength2()) * BASE_CARD_STRENGTH.pow(index).toInt()}
         .sum()
 
     private fun Card.strength2(): Int {
-        return if (this.label < Label.J) this.label.ordinal + 1
-        else if (this.label > Label.J) this.label.ordinal
+        return if (this < Card.J) this.ordinal + 1
+        else if (this > Card.J) this.ordinal
         else 0
     }
 
-
-    enum class Label {
+    enum class Card {
         TWO,
         THREE,
         FOUR,
@@ -137,21 +120,21 @@ object Day07 {
         A,
     }
 
-    private fun Char.toLabel(): Label {
-        return when (this) {
-            'A' -> Label.A
-            'K' -> Label.K
-            'Q' -> Label.Q
-            'J' -> Label.J
-            'T' -> Label.TEN
-            '9' -> Label.NINE
-            '8' -> Label.EIGHT
-            '7' -> Label.SEVEN
-            '6' -> Label.SIX
-            '5' -> Label.FIVE
-            '4' -> Label.FOUR
-            '3' -> Label.THREE
-            else -> Label.TWO
+    private fun Card(char: Char): Card {
+        return when (char) {
+            'A' -> Card.A
+            'K' -> Card.K
+            'Q' -> Card.Q
+            'J' -> Card.J
+            'T' -> Card.TEN
+            '9' -> Card.NINE
+            '8' -> Card.EIGHT
+            '7' -> Card.SEVEN
+            '6' -> Card.SIX
+            '5' -> Card.FIVE
+            '4' -> Card.FOUR
+            '3' -> Card.THREE
+            else -> Card.TWO
         }
     }
 
@@ -164,7 +147,6 @@ object Day07 {
         FOUR_OF_A_KIND,
         FIVE_OF_A_KIND
     }
-    data class Card(val label: Label)
     data class Hand(val bid: Int, val cards: List<Card>)
 }
 
